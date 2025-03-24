9065174872e08a61f7ed42edbdfd8c18986e8dce
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2025-03-24 23:37:57 +0000

    Implement basic discrete logarithms for Dirichlet characters

--- dirichlet_character.m
+++ dirichlet_character.m
@@ -22,3 +22,50 @@ intrinsic EulerPhi(M :: RngUPolElt[FldFin]) -> RngIntElt
   require M ne 0: "The modulus M is the zero polynomial.";
   return EulerPhiWithF(Factorization(M));
 end intrinsic;
+
+function LogWithPhi(phi, b, x)
+  b_n := 1;
+  for n := 0 to phi - 1 do
+    if b_n eq x then
+      return n;
+    end if;
+    b_n *:= b;
+    if b_n eq 0 then
+      return 0; // the base b is not a unit
+    end if;
+    if b_n eq 1 then
+      return -1; // the base b is not a generator
+    end if;
+  end for;
+  return -2; // the element x is not a unit
+end function;
+
+intrinsic Log(b :: RngUPolResElt[FldFin], x :: RngUPolResElt[FldFin])
+  -> RngIntElt
+{ The discrete logarithm log_b(x) for a base b and an element x in the unit
+  group of k[t] / M for a non-zero irreducible polynomial M in k[t]. This is the
+  smallest positive integer n such that b^n = x. }
+  M := Modulus(Parent(b));
+  require IsIrreducible(M): "The modulus M is not irreducible.";
+  n := LogWithPhi(EulerPhi(M), b, x);
+  require n ne 0: "The base b is not a unit.";
+  require n ne -1: "The base b is not a generator.";
+  require n ne -2: "The element x is not a unit.";
+  return n;
+end intrinsic;
+
+intrinsic Log(b :: RngUPolResElt[FldFin], x :: RngUPolElt[FldFin]) -> RngIntElt
+{ " }
+  return Log(b, Parent(b) ! x);
+end intrinsic;
+
+intrinsic Log(b :: RngUPolElt[FldFin], x :: RngUPolResElt[FldFin]) -> RngIntElt
+{ " }
+  return Log(Parent(x) ! b, x);
+end intrinsic;
+
+intrinsic Log(M :: RngUPolElt[FldFin], b :: RngUPolElt[FldFin],
+    x :: RngUPolElt[FldFin]) -> RngIntElt
+{ " }
+  return Log(quo<Parent(M) | M> ! b, x);
+end intrinsic;
